4af62ad1ff9d401d4a1b5b06d8fb7b18fbedf078
Isaac <ipambrozevicius@gmail.com>
2025-05-15 09:06:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Redes_ac01',
+  webDir: 'www'
+};
+
+export default config;
